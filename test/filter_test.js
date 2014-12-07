a9d466641d48a84b66c977554d366c18f33a5f07
Tim Rodger <tim.rodger@gmail.com>
2014-12-07 14:39:08 +0000

    Adds test for filter.remove

--- test/filter_test.js
+++ test/filter_test.js
@@ -3,10 +3,19 @@ var filter = require('../src/lib/filter'),
 
 describe('Filter', function() {
     describe('match', function() {
-        it('should respond with an array', function() {
+        it('should respond with an array for no input', function() {
             var value = [];
             var pattern = '';
             filter.match(value, pattern, function(result) {
+                assert.equal(0, result.length);
+            });
+        });
+    });
+    describe('remove', function() {
+        it('should respond with an array for no input', function() {
+            var value = [];
+            var pattern = '';
+            filter.remove(value, pattern, function(result) {
                 assert.equal(0, result.length);
             });
         });
