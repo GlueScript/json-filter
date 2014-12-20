ea286fd7e6f977217586ee8f97b4571dae1789b3
Tim Rodger <tim.rodger@gmail.com>
2014-12-20 10:13:03 +0000

    look at using _ more

--- src/lib/filter.js
+++ src/lib/filter.js
@@ -5,17 +5,21 @@ var _ = require('underscore');
  * input is an array of objects, or a single object
  * pattern in an object
  * callback expects two args, err and result
+ *
+ * can _.partition(array, predicate) be used by both functions?
  */
 exports.keep = function(input, pattern, callback) {
 
     var output = [];
 
     // ensure that input is an array, if not treat it as a single element array
-    if (!(input instanceof Array)) {
+    if (!_.isArray(input)) {
         input = [input];
     }
     
     // add all matching elements to output
+    // _.where() will work unless pattern allows for wild card matching
+    // or just use _.filter()?
     _.each(pattern, function(val, key, list) {
         output = output.concat(_.filter(input, function(item){
             return item[key] == val;
@@ -34,11 +38,12 @@ exports.keep = function(input, pattern, callback) {
 exports.remove = function(input, pattern, callback) {
 
     // ensure that input is an array, if not treat it as a single element array
-    if (!(input instanceof Array)) {
+    if (!_.isArray(input)) {
         input = [input];
     }
 
     // keep only items that don't match anything in pattern
+    // reimplement using _.reject
     _.each(pattern, function(val, key, list) {
         input = _.filter(input, function(item){
             return item[key] != val;
